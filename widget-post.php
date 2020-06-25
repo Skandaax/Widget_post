7fa23e72c2159ab511b6f92c4c76eaa0885a17e1
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-06-25 15:10:31 +0200

    Ajout des fichiers index.php,licence et readme.txt & widget_post

---
+++ widget-post.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+Plugin Name: Widget post !
+Plugin URI: https://www.inforaz.com/developpement-web-web-mobile/
+Description: Widget permettant l'affichage d'une liste d'articles.
+Author: Couillin Yannick
+Author URI: http:/www.inforaz.com/
+Version 1.0
+Licence: GPLv2
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright {2020} {Yannick_Couillin} {email : {ameb@inforaz.com}}
+*/
